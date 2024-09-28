1fe540b28cc8c712190e4534d8883f5f29137838
Niyonsenga-Belyse <b.niyonseng@alustudent.com>
2024-09-28 10:55:45 +0200

    DBStorage - States and Cities

---
+++ models/engine/db_storage.py
@@ -0,0 +1,47 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from models.base_model import Base
+import os
+
+class DBStorage:
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """Initialize the DBStorage engine"""
+        user = os.getenv('HBNB_MYSQL_USER')
+        pwd = os.getenv('HBNB_MYSQL_PWD')
+        host = os.getenv('HBNB_MYSQL_HOST')
+        db = os.getenv('HBNB_MYSQL_DB')
+        env = os.getenv('HBNB_ENV')
+
+        self.__engine = create_engine(f'mysql+mysqldb://{user}:{pwd}@{host}/{db}', pool_pre_ping=True)
+
+        if env == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """Query the current database session"""
+        # Implementation for querying the database
+        ...
+
+    def new(self, obj):
+        """Add the object to the current database session"""
+        self.__session.add(obj)
+
+    def save(self):
+        """Commit all changes of the current session"""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """Delete obj from the current database session"""
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """Reload the database"""
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
+
